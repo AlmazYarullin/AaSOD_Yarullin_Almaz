2f216285dfb962d7a12ca2f36ff0ed27ac07e55b
Pumpka <almas2901@yandex.ru>
2021-04-20 20:00:35 +0300

    Initial commit

--- Semestrovka/HashTable.cpp
+++ Semestrovka/HashTable.cpp
@@ -12,28 +12,27 @@ struct HashTable
 {
     int max_size = 1;
     int size = 0;
-    list<string> * lst = new list<string>[max_size];
+    list<string> * arr = new list<string>[max_size];
 
-    int hash(string str) {
+    int hash(string key) {
         int hash = 0;
-        for (int i = 0; i < str.length(); i++) {
-            hash += str[i];
+        for (int i = 0; i < key.length(); i++) {
+            hash += key[i];
         }
         hash = abs(hash * size) % max_size;
-        // if (size > 944900) cout << "hash: " << hash << endl;
         return hash;
     }
 
     void rehash() {
-        list<string> * old_lst = lst;
+        list<string> * old_arr = arr;
         int old_size = max_size;
         max_size *= 2;
-        lst = new list<string>[max_size];
+        arr = new list<string>[max_size];
         for (int i = 0; i < old_size; i++) {
-            if (old_lst[i].size() > 0) {
-                for (list<string>::iterator it = old_lst[i].begin(); it != old_lst[i].end(); it++) {
-                    int key = hash(*it);
-                    lst[key].push_back(*it);
+            if (old_arr[i].size() > 0) {
+                for (list<string>::iterator it = old_arr[i].begin(); it != old_arr[i].end(); it++) {
+                    int idx = hash(*it);
+                    arr[idx].push_back(*it);
                 }
             }
         }
@@ -43,27 +42,24 @@ struct HashTable
         if (size >= max_size * 0.8) {
             rehash();
         }
-        int key = hash(str);
+        int idx = hash(str);
         bool flag = true;
-        for (list<string>::iterator it = lst[key].begin(); it != lst[key].end(); it++) {
+        for (list<string>::iterator it = arr[idx].begin(); it != arr[idx].end(); it++) {
             if (*it == str) {
                 flag = false;
                 break;
             }
         }
         if (flag) {
-            lst[key].push_back(str);
+            arr[idx].push_back(str);
             size++;
-            // if (size > 944900) {
-            //     cout << "size: " << size << " " << max_size << " " << str << endl;
-            // }
         }
     }
 
     bool find(string str) {
-        int key = hash(str);
+        int idx = hash(str);
         bool flag = false;
-        for (list<string>::iterator it = lst[key].begin(); it != lst[key].end(); it++) {
+        for (list<string>::iterator it = arr[idx].begin(); it != arr[idx].end(); it++) {
             if (*it == str) {
                 flag = true;
                 break;
@@ -73,78 +69,12 @@ struct HashTable
     }
 
     void del(string str) { 
-        int key = hash(str);
-        for (list<string>::iterator it = lst[key].begin(); it != lst[key].end(); it++) {
+        int idx = hash(str);
+        for (list<string>::iterator it = arr[idx].begin(); it != arr[idx].end(); it++) {
             if (*it == str) {
-                lst[key].erase(it);
+                arr[idx].erase(it);
                 break;
             }
         }
     }
-
 };
-
-
-int test(int fileNum, int filesAmount, string fileName) {
-    ofstream fout;
-    cout << "file: " << fileNum + 1 << "/" << filesAmount << endl;
-    ifstream fin;
-    HashTable * words = new HashTable;
-    int averageTime = -1;
-    string strAmount;
-    int start, end;
-
-    fin.open("test_data\\data" + to_string(fileNum) + ".txt");
-    if (fin.is_open()) {
-        fin >> strAmount;
-        int wordsAmount = stoi(strAmount);
-        start = clock();
-        while (!fin.eof()) {
-            string word;
-            fin >> word;
-            words->append(word);
-        }
-        end = clock();
-        fin.close();
-
-        // int times;
-        // for (int i = 0; i < TESTS_AMOUNT; i++) {
-        //     int start = clock();
-        //     words->append("hereWego!");
-        //     int end = clock();
-        //     times += end - start;
-        // }
-        // averageTime = times / TESTS_AMOUNT;
-    }
-    else {
-        cout << "OPEN ERROR!";
-        return 999;
-    }
-
-    fout.open("" + fileName, ios::app);
-    cout << end- start << endl;
-    fout << strAmount << ';' << to_string(end - start) << '\n';
-    fout.close();
-    delete words;
-    return 0;
-}
-
-
-
-int main() {
-    const int FILES_AMOUNT = 101;
-    const string FILENAME = "test3.csv";
-    ofstream fout;
-    // fout.open("Semestrovka\\" + FILENAME);
-    // if (fout.is_open()) {
-    //     fout << "amount;time\n";
-    // }
-    int fileNum;
-    cin >> fileNum;
-    test(fileNum, FILES_AMOUNT, FILENAME);
-    // for (int fileNum = 0; fileNum < FILES_AMOUNT; fileNum++) {
-    //     if (test(fileNum, FILES_AMOUNT, FILENAME) != 0) {
-    //         return 999;
-    //     }
-    // }
-}
